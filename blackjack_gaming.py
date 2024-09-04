1471ed5921e3b6292421cd1bf5beb6dbdca72354
Jerry-gifspec <sjerdiv@gmail.com>
2024-09-04 10:14:19 +0900

    Add files via upload

---
+++ blackjack_gaming.py
@@ -0,0 +1,22 @@
+n = 5
+digit = 3
+
+def pyramid_of_repeating_digits(n, digit):
+    current_number = 0
+    
+    for i in range(2, n + 2):
+        current_number = current_number * 10 + digit
+        print(current_number)
+
+pyramid_of_repeating_digits(n, digit)
+
+def pyramid_of_repeating_digits(n, digit):
+    current_number = 0
+    
+    for i in range(2, n + 2):
+        current_number = current_number * 10 + digit
+        print(current_number)
+
+pyramid_of_repeating_digits(n, digit)
+
+
